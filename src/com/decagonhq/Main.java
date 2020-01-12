3fe368e267ed29e593f2299ebe14759ca3ad1bac
Jideofor Nwoke <njusticej@gmail.com>
2020-01-12 09:37:14 +0100

    add testing the bank class

--- src/com/decagonhq/Main.java
+++ src/com/decagonhq/Main.java
@@ -3,6 +3,26 @@ package com.decagonhq;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Bank bank = new Bank("AccessBank");
+	bank.addBranch("lekki");
+	bank.addCustomer("lekki", "jide", 89.00);
+	bank.addCustomer("lekki", "ofor", 89.00);
+	bank.addCustomer("lekki", "joel", 89.00);
+
+	bank.addBranch("ijesha");
+	bank.addCustomer("ijesha", "mike", 89.00);
+	bank.addCustomerTransaction("lekki", "jide", 300000);
+	bank.addCustomerTransaction("lekki", "jide", 300000);
+	bank.addCustomerTransaction("lekki", "jide", 300000);
+
+	bank.listCustomers("ijesha", true);
+
+//	if(!bank.addCustomer("just", "jide", 80.00)){
+//		System.out.println("ok");
+//	}
+
+		if(!bank.addBranch("ijesha")){
+			System.out.println("ok");
+		}
     }
 }
